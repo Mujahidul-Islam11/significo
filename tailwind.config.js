63a14f5f623f91b908c213edc74fc011fec945f3
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-08-21 11:53:24 +0600

    Landing page team list section completed

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+      './src/**/*.{html,js,jsx,ts,tsx,vue}', // Make sure these paths match your file structure
+      './public/index.html',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
